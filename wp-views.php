c7ca52229654e87d7b21830106a873e6dfebe228
dagment <dagment@mail.ua>
2017-12-20 21:14:10 +0200

    Add main files

---
+++ wp-views.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP Views
+ * Description: This plugin add custom views for posts
+ * Version: 0.0.1
+ * Author: Mykhailo Prokopiuk and Ustym Radobenko
+ */
+defined( 'ABSPATH' ) or exit;
+define('wpviews_path',plugin_dir_path(__FILE__ ));
+define('wpviews_plugin_file_path',__FILE__);
+
+
+
+
+
+//require_once my_path ."billing-multiple.php";
