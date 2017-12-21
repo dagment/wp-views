5bf9ce77c3a6da3550d93534a4e49bf6a4ba397b
dagment <dagment@mail.ua>
2017-12-21 21:04:53 +0200

    Add admin menus

--- wp-views.php
+++ wp-views.php
@@ -9,6 +9,11 @@ defined( 'ABSPATH' ) or exit;
 define('wpviews_path',plugin_dir_path(__FILE__ ));
 define('wpviews_plugin_file_path',__FILE__);
 
+//function wpv_register_options(){
+//
+//}
+//register_activation_hook(wpviews_plugin_file_path, 'wpv_register_options' );
+
 
 
 require_once wpviews_path ."/includes/admin.php";
